b43f43bfbeb27be8ffdb717c5613d6fa36aadfb0
Chris Wiggins <me@cwiggs.com>
2022-09-08 12:09:12 -0700

    add basic test

---
+++ main_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "net/http"
+    //"net/http/httptest"
+    "testing"
+)
+
+func TestGetPodcasts(t *testing.T) {
+    _, err := http.NewRequest(http.MethodGet, "/podcasts", nil)
+    if err != nil {
+        t.Fatal(err)
+    }
+    /*
+    w := httptest.NewRecorder()
+    RequestHandler(w, req)
+    result := w.Result()
+    defer result.Body.Close()
+
+    data, err := ioutil.ReadAll(result.Body)
+
+    if err != nil {
+        t.Errorf("Error: %v", err)
+    }
+
+    if string(data) != expected {
+        t.Errorf("Expected Joe Rogan but got %v", string(data))
+    }
+    */
+}
