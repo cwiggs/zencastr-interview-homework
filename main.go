c28be2103f562767abbcd9511261b940dc445ec1
Chris Wiggins <me@cwiggs.com>
2022-09-09 10:09:10 -0700

    go fmt

--- main.go
+++ main.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "encoding/json"
+	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 )
 
 type Podcast struct {
-	Name string `json:"Name"`
+	Name   string `json:"Name"`
 	Author string `json:"Author"`
 }
 
 func PodcastHandler(w http.ResponseWriter, r *http.Request) {
-    podcasts := []Podcast{
-        Podcast{"JRE", "Joe Rogan"},
-        Podcast{"CWE", "Chris Wiggins"},
-    }
+	podcasts := []Podcast{
+		Podcast{"JRE", "Joe Rogan"},
+		Podcast{"CWE", "Chris Wiggins"},
+	}
 
-    json.NewEncoder(w).Encode(podcasts)
+	json.NewEncoder(w).Encode(podcasts)
 }
 
 func main() {
-    http.HandleFunc("/podcasts", PodcastHandler)
-    fmt.Println("Starting http server")
-    log.Fatal(http.ListenAndServe(":1234", nil))
+	http.HandleFunc("/podcasts", PodcastHandler)
+	fmt.Println("Starting http server")
+	log.Fatal(http.ListenAndServe(":1234", nil))
 }
--- main_test.go
+++ main_test.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-    "net/http"
-    //"net/http/httptest"
-    "testing"
+	"net/http"
+	//"net/http/httptest"
+	"testing"
 )
 
 func TestGetPodcasts(t *testing.T) {
-    _, err := http.NewRequest(http.MethodGet, "/podcasts", nil)
-    if err != nil {
-        t.Fatal(err)
-    }
-    /*
-    w := httptest.NewRecorder()
-    RequestHandler(w, req)
-    result := w.Result()
-    defer result.Body.Close()
+	_, err := http.NewRequest(http.MethodGet, "/podcasts", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	/*
+	   w := httptest.NewRecorder()
+	   RequestHandler(w, req)
+	   result := w.Result()
+	   defer result.Body.Close()
 
-    data, err := ioutil.ReadAll(result.Body)
+	   data, err := ioutil.ReadAll(result.Body)
 
-    if err != nil {
-        t.Errorf("Error: %v", err)
-    }
+	   if err != nil {
+	       t.Errorf("Error: %v", err)
+	   }
 
-    if string(data) != expected {
-        t.Errorf("Expected Joe Rogan but got %v", string(data))
-    }
-    */
+	   if string(data) != expected {
+	       t.Errorf("Expected Joe Rogan but got %v", string(data))
+	   }
+	*/
 }
